6ef32d600d240e8b28779d3c12c74634b43bd88b
DekarN <nikolaydekar@gmail.com>
2023-09-14 18:54:47 +0300

    D6.6

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Post
+
+menu = ["Главная страница", "Посты"]
+
+def index(request):
+    return render(request, 'news/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+def posts(request):
+    posts = Post.objects.order_by('-created')
+    return render(request, 'news/posts.html', {'posts': posts})
+
+class NewsDetailView(DetailView):
+    model = Post
+    template_name = 'news/details_view.html'
+    context_object_name = 'post'
+
+def post(request, post_id):
+    return HttpResponse(f'Пост {post_id}')
